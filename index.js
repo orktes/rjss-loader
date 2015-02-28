ce79803ea5e33ee719ee23fd7e7a73e566323a2f
Jaakko Lukkari <jaakko@applifier.com>
2015-02-28 15:51:41 +0200

    Initial version of rjss-loader

---
+++ index.js
@@ -0,0 +1,28 @@
+var RJSS = require('rjss');
+var loaderUtils = require('loader-utils');
+
+var rjss;
+
+module.exports = function(source) {
+  this.cacheable && this.cacheable();
+
+  if (!rjss) {
+    rjss = new RJSS({sourceMap: true});
+  }
+
+  var sourceFilename = loaderUtils.getRemainingRequest(this);
+  var current = loaderUtils.getCurrentRequest(this);
+
+  var transform = rjss.parseContent(source, {
+    path: this.resource
+  });
+
+  var sourceMap = JSON.parse(transform.getSourceMap());
+
+  if (sourceMap) {
+    sourceMap.sources = [sourceFilename];
+    sourceMap.file = current;
+    sourceMap.sourcesContent = [source];
+  }
+  this.callback(null, transform.getCode(), sourceMap);
+};
